2933a5ade96b0d4c741d6e6338354d272e2e3f8e
MMartin1013 <manomartin@Manos-MacBook-Pro.local>
2021-05-20 23:01:34 -0400

    Added categories their messages

---
+++ script.js
@@ -0,0 +1,8 @@
+//Mixed messages ot be printed ot the console
+const messages = {
+    references : ["Spongebob","Avatar","Adventure Time"],
+    jokes : ["What did the Triangle say to the Circle? You're so pointless! ;)", "Why does a moon rock taste better than an earth rock? Because its meteor! ;)","Why can't a nose be 12 inches? Because then it would be a foot! ;)"],
+    advice : ["Don't let your efforts be for nothing","Knowledge is nothing without action", "Don't forget to give yourself a break"]
+
+}
+
